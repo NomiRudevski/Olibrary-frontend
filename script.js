2e9927a6388f0a099df42d75c3aa343a62150007
NomiRudevski <nomi.rudevski@gmail.com>
2024-07-08 20:18:29 +0300

    server link

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const SEREVER = "http://127.0.0.1:5000"
+const SEREVER = "https://olibrary-backend.onrender.com"
 
 const check_late = () => axios.get(SEREVER + '/cheak-if-late')
 const showMenu = () => document.getElementById("dropdownMenu").style.visibility = 'visible';
